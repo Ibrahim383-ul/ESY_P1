8bfd1ebcaf4d2a8e81e1232ce119c46611599be5
Ibrahim383-ul <ibrahimalsaeed688@icloud.com>
2026-01-05 14:16:25 +0100

    Version_1.1/ P1

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
+/*
+* File:        main.cpp P1
+* Autor:       Ibrahim AL SAEED
+* Datum:       03.01.2026
+* Version:     1.0
+* Lizenz:      MIT
+
+* Description: Dieses Programm ist ein einfacher Türscanner, wie er in Schulen, Firmen usw. verwendet werden kann.
+*/
+
 // TODO Fileheader fehlt
 // TODO Flussdiagramm fehlt
 // Kein separates Unterverzeichnis benötigt - habe ich bereits entfernt - befindet sich jetzt im Hauptverzeichnis des Repos
 // TODO Die Readme hat mich ein wenig verwirrt - Sie verwenden einen Arduino Nano?
 // Freue mich schon auf die Livedemo!
+
+
 
 #include <SPI.h>
 #include <MFRC522.h>
@@ -14,8 +26,8 @@
 MFRC522 rfid(SS_PIN, RST_PIN);
 Servo myServo;
 
-const int servoPin = 7;	// TODO bei PINs sollte es eine const Variable sein oder ein #define
-const int piezopin = 6;
+#define servoPin 7	// TODO bei PINs sollte es eine const Variable sein oder ein #define
+#define piezopin 6
 
 // Erlaubte RFID-Karte
 byte allowedUID[4] = {0x33, 0xDF, 0x6D, 0xE2};
@@ -23,76 +35,75 @@ byte allowedUID[4] = {0x33, 0xDF, 0x6D, 0xE2};
 
 void setup()
 {
-    Serial.begin(9600);
-    SPI.begin();
-    rfid.PCD_Init();
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.PCD_Init();
 
-    myServo.attach(servoPin);
-    myServo.write(0);
+  myServo.attach(servoPin);
+  myServo.write(0);
 
-    pinMode(piezopin, OUTPUT);
+  pinMode(piezopin, OUTPUT);
+  pinMode(5, OUTPUT);   
+  pinMode(6, OUTPUT);   
 
-    pinMode(5, OUTPUT);   // Grün LED
-    pinMode(6, OUTPUT);   // Rot LED
-
-    Serial.println("RFID Access Control bereit...");
+  Serial.println("RFID Access Control bereit...");
 }
 
 
 void loop()
 {
-    // Warten auf neue Karte
-    if (!rfid.PICC_IsNewCardPresent()) 
-        return;
+  // Warten auf neue Karte
+  if (!rfid.PICC_IsNewCardPresent()) 
+  return;
 
-    if (!rfid.PICC_ReadCardSerial()) 
-        return;
+  if (!rfid.PICC_ReadCardSerial()) 
+  return;
 
-    Serial.print("Karte erkannt. UID: ");
+  Serial.print("Karte erkannt. UID: ");
 
-    bool match = true;
+  bool match = true;
 
-    // UID vergleichen
-    for (byte i = 0; i < rfid.uid.size; i++)
+  // UID vergleichen
+  for (byte i = 0; i < rfid.uid.size; i++)
+  {
+    Serial.print(rfid.uid.uidByte[i], HEX);
+    Serial.print(" ");
+
+    if (rfid.uid.uidByte[i] != allowedUID[i])
     {
-        Serial.print(rfid.uid.uidByte[i], HEX);
-        Serial.print(" ");
-
-        if (rfid.uid.uidByte[i] != allowedUID[i])
-        {
-            match = false;
-        }
+     match = false;
     }
-    Serial.println();
+  }
+
+  Serial.println();
 
 
-    // Entscheidung
-    if (match)
-    {
-        Serial.println("ZUGRIFF ERLAUBT!");
+  // Entscheidung
+  if (match)
+  {
+    Serial.println("ZUGRIFF ERLAUBT!");
+    digitalWrite(5, HIGH);   
+    digitalWrite(6, LOW);    
+    myServo.write(90);       
+  }
 
-        digitalWrite(5, HIGH);   // Grün
-        digitalWrite(6, LOW);    // Rot aus
+  else
 
-        myServo.write(90);       // Öffnen
-    }
-    else
-    {
-        Serial.println("ZUGRIFF VERWEIGERT!");
-
-        digitalWrite(5, LOW);
-        digitalWrite(6, HIGH);   // Rot ein
-
-        tone(piezopin, 400, 200);
-    }
+  {
+    Serial.println("ZUGRIFF VERWEIGERT!");
+    digitalWrite(5, LOW);
+    digitalWrite(6, HIGH);   
+    tone(piezopin, 400, 200);
+  }
 
 
-    // Karte abmelden
-    rfid.PICC_HaltA();
-    rfid.PCD_StopCrypto1();
+  // Karte abmelden
+  rfid.PICC_HaltA();
+  rfid.PCD_StopCrypto1();
 
-    delay(1500);
+  delay(1500);
 
-    // Servo zurücksetzen
-    myServo.write(0);
+  // Servo zurücksetzen
+ 
+  myServo.write(0);
 }
